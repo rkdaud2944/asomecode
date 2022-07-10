a6015330aeec223f72a101964f2b087d56cd5a0e
Ryu <ryujt658@gmail.com>
2022-07-10 21:45:51 +0900

    /lesson/list 구현 완료 (백엔드는 모두 일괄적으로 다신 티켓에서 처리할 예정)

--- src/views/lesson/ListView.vue
+++ src/views/lesson/ListView.vue
@@ -1,4 +1,4 @@
-<template>    
+<template>
     <el-row class="header">
         <el-col :span="10">
             <img src="/images/common/logo_con.jpg" />
@@ -18,9 +18,9 @@
                 {{ $route.query.description }}
             </el-row>
 
-            <el-row v-for="(lesson, index) in lessons" :key="index">
-                <el-col :span="4">{{ index + 1 }}차시</el-col>
-                <el-col :span="20">{{ lesson.title }}</el-col>                
+            <el-row @click="onLessonClick(lesson)" class="list" v-for="(lesson, index) in lessons" :key="index">
+                <el-col :span="2" class="list-left" >{{ index + 1 }}차시</el-col>
+                <el-col :span="21" class="list-main" >{{ lesson.title }}</el-col>
             </el-row>
         </el-col>
     </el-row>
@@ -32,32 +32,44 @@ export default {
         return {
             lessons: [
                 {
+                    id: 0,
                     title: '어썸키트 과정 1',
                     description: '최소한의 부품과 장치를 이용하여 교구의 비용은 줄이고 피지컬 컴퓨팅 전반의 코딩을 체험할 수 있도록 준비된 교육 프로그램',
                 },
                 {
+                    id: 1,
                     title: '어썸키드 과정 2',
                     description: '각종 부품과 센서에 대해 이해하고 간단한 놀잇감을 직접 개발해보면서 생활 속에서 만나는 다양한 장치의 원리를 이해하도록 개발된 프로그램',
                 },
                 {
-                    title: '어썸봇 과정 1',
-                    description: '스마트폰으로 조종하는 2족로봇을 직접 개발하고 개발된 놀잇감을 프로그램을'
+                    id: 2,
+                    title: '어썸키드 과정 2',
+                    description: '각종 부품과 센서에 대해 이해하고 간단한 놀잇감을 직접 개발해보면서 생활 속에서 만나는 다양한 장치의 원리를 이해하도록 개발된 프로그램',
                 },
             ],
         }
     },
 
     setup() {
-        
+
     },
 
     mounted() {
         console.log(this.$route.query);
     },
+
+    methods: {
+        onLessonClick(lesson) {
+            this.goTo(`/lesson/detail/${lesson.id}`);
+        },
+    },
 }
 </script>
 
 <style scoped>
+.el-col {
+    border-radius: 8px;
+}
 .header {
     width: 100%;
     background: rgb(3, 84, 87);
@@ -72,6 +84,28 @@ export default {
     padding: 4px;
 }
 .list-header {
+    margin-top: 20px;
     margin-bottom: 20px;
+}
+.list {
+    cursor: pointer;
+}
+.list-left {
+    width: 100%;
+    height: 32px;
+    background: #D3DCE6;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.list-main {
+    height: 32px;
+    background: #D3DCE6;
+    padding-left: 20px;
+    margin: 10px;
+    display: flex;
+    align-items: center;
 }
 </style>
