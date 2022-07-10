d6f3cdb5cf90d83d3db6ceba763979d3271c5cf1
Ryu <ryujt658@gmail.com>
2022-07-10 13:14:56 +0900

    help 탭 콤포넌트 작업

--- src/views/subject/ListView.vue
+++ src/views/subject/ListView.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>
-        
-    </div>
+    <el-tabs type="border-card">
+        <el-tab-pane label="pin">핀 라이브러리</el-tab-pane>
+        <el-tab-pane label="module">모듈 라이브러리</el-tab-pane>
+        <el-tab-pane label="qna">Q&A</el-tab-pane>
+        <el-tab-pane label="subject">커스텀 과목</el-tab-pane>
+    </el-tabs>
+
+    <div></div>
 </template>
