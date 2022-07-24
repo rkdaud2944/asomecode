47345c0b6365a3e43d77aa64c80c410eba02932e
Ryu <ryujt658@gmail.com>
2022-07-24 13:25:39 +0900

    lesson 제목 클릭 시 해당 위치로 이동

--- src/utils/markdown.js
+++ src/utils/markdown.js
@@ -1,19 +1,37 @@
+/* eslint-disable */
+
 export default {
-    getTitles(markdown) {
-        const titles = [];
-        const lines = markdown.split("\n");
+    setHtml(text) {
+        this.html = marked.parse(text);
+
+        this.titles = [];
+        const lines = text.split("\n");
         for (let i = 0; i < lines.length; i++) {
             const line = lines[i].trim();
-            if (line.startsWith("#")) {
-                const title_items = line.split(" ");
-                const level = title_items[0];
-                title_items.shift();
-                titles.push({
-                    name: title_items.join(" "),
-                    level: level.length,
-                });
-            }
+            if (!line.startsWith("#")) continue;
+
+            const tag = "tag-" + i;
+            const title_items = line.split(" ");
+            const level = title_items[0];
+
+            title_items.shift();
+            const name = title_items.join(" ");
+
+            this.html = this.html.replace(name, `<div id="${tag}">${name}</div>`);
+
+            this.titles.push({
+                name: name,
+                level: level.length,
+                tag: tag,
+            });
         }
-        return titles;
+    },
+
+    getTitles() {
+        return this.titles;
+    },
+
+    getContent() {
+        return this.html;
     }
 }
--- src/views/lesson/LessonDetail.vue
+++ src/views/lesson/LessonDetail.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="row">
         <div class="col-3">
-            <h4  class="flex flex-center">목차</h4>
-            <div :class="`title title-${title.level}`" v-for="(title, index) in titles" :key="index">{{ title.name }}</div>
+            <h4 class="flex flex-center">목차</h4>
+            <div @click="moveTo(title.tag)" :class="`title title-${title.level}`" v-for="(title, index) in titles" :key="index">
+                {{title.name}}
+            </div>
         </div>
 
         <div class="col-9">
@@ -12,10 +14,8 @@
 </template>
 
 <script>
-/* eslint-disable */
-
-import markdown from '@/utils/markdown';
-import lesson03 from '@/data/lesson03.js';
+import markdown from "@/utils/markdown";
+import lesson03 from "@/data/lesson03.js";
 
 export default {
     data() {
@@ -26,29 +26,44 @@ export default {
     },
 
     mounted() {
+        markdown.setHtml(lesson03);
         this.titles = markdown.getTitles(lesson03);
-        this.content = marked.parse(lesson03);
+        this.content = markdown.getContent();
     },
-}
+
+    methods: {
+        moveTo(tag) {
+            const element = window.document.getElementById(tag);
+            console.log(tag, element);
+            const top = element.offsetTop;
+            window.scrollTo(0, top);
+        },
+    },
+};
 </script>
 
 <style scoped>
 .title {
     width: 100%;
     margin: 8px;
+    cursor: pointer;
 }
+
 .title-1 {
     font-size: 1.5em;
     font-weight: bold;
 }
+
 .title-2 {
     font-size: 1.2em;
     padding-left: 10px;
 }
+
 .title-3 {
     font-size: 1em;
     padding-left: 20px;
 }
+
 .title-4 {
     font-size: 0.8em;
     padding-left: 30px;
