b597005d5f76b2bee2f727b6278424d198f9fb0a
Ryu <ryujt658@gmail.com>
2022-08-28 18:50:09 +0900

    시리얼 모듈 기본 동작 테스트 완료

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -4,12 +4,12 @@
             <q-btn @click="connect()" color="primary" label="connect" class="q-ml-md" />
             <q-btn @click="goTo('/')" color="black" label="home" class="q-ml-md" />
             <q-btn @click="openUrl('/editor')" color="secondary" label="editor" class="q-ml-md" />
-            <q-btn @click="sendCode('stop')" color="deep-orange" label="stop" class="q-ml-md" />
+            <q-btn @click="stop()" color="deep-orange" label="stop" class="q-ml-md" />
         </div>
         <div class="on-right">
-            <q-btn @click="sendCode('reboot')" color="secondary" label="reboot" class="q-ml-md" />
-            <q-btn @click="sendCode('format')" color="deep-orange" label="format" class="q-ml-md" />
-            <q-btn @click="sendCode('update')" color="brown" label="update" class="q-ml-md" />
+            <q-btn @click="reboot()" color="secondary" label="reboot" class="q-ml-md" />
+            <q-btn @click="format()" color="deep-orange" label="format" class="q-ml-md" />
+            <q-btn @click="listFiles()" color="brown" label="update" class="q-ml-md" />
             <q-btn @click="openUrl('/help')" color="purple" label="HELP" class="q-ml-md" />
         </div>
     </div>
@@ -23,12 +23,15 @@ export default {
     mixins: [VueBase],
 
     methods: {
+        ...serial,
+
         connect() {
-            serial.list();
-            // serial.connect("COM1");
+            // serial.list();
+            serial.connect();
         },
+
         sendCode() {
-            // serial.sendCode(code);
+            serial.writeLn("print('Hi')");
         }
     }
 }
--- src/utils/serial.js
+++ src/utils/serial.js
@@ -4,19 +4,23 @@ let port = null;
 let parser = null;
 
 export default {
-    connect(path) {
-        try {
-            port = new SerialPort({
-                path: path,
-                baudRate: 115200,
-                lock: false,
-            });
+    async connect() {
+        if (port !== null) this.disconnect();
 
-        } catch (error) {
-            console.log(error);
-            port = null;
+        const asomeboard = await this.getAsomeboard();
+        console.log(asomeboard);
+        if (asomeboard == null) {
+            // TODO: 오류 메시지 출력
+            console.log("asomeboard not found");
             return;
         }
+
+        port = new SerialPort({
+            path: asomeboard.path,
+            baudRate: 115200,
+            autoOpen: false,
+            lock: false,
+        });
 
         parser = new ReadlineParser({ delimiter: '\r\n' });
         port.pipe(parser);
@@ -27,16 +31,25 @@ export default {
             console.log('Seiral.closed');
             this.terminate();
         });
-
         port.on('error', (err) => {
             console.error("Seiral.error", err);
             this.terminate();
         });
-
         port.on('connected', (err) => {
             console.log('Seiral.connected', err);
             this.terminate();
         });
+
+        try {
+            port.open();
+            // TODO: 이벤트 처리
+            console.log('Seiral.open');
+        } catch (error) {
+            // TODO: 오류 메시지 출력
+            console.log(error);
+            port = null;
+            return;
+        }
     },
 
     disconnect() {
@@ -54,11 +67,60 @@ export default {
     },
 
     list() {
-        SerialPort.list((err, ports) => {
-            console.log("list", ports);
-            ports.forEach(port => {
-                console.log(port);
-            });
-        });
+        SerialPort.list().then(console.log);
+    },
+
+    write(text) {
+        if (port == null) return;
+        try {
+            port.write(text);
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    writeLn(text) {
+        if (port == null) return;
+        try {
+            port.write(text +"\r\n");
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    stop() {
+        this.write(String.fromCharCode(3));
+    },
+
+    reboot() {
+        this.writeLn("import machine; machine.reset()");
+    },
+
+    format() {
+        this.writeLn("import os; import flashbdev; os.VfsFat.mkfs(flashbdev.bdev)");
+        this.writeLn("import machine; machine.reset()");
+    },
+
+    listFiles() {
+        this.write(codeListFiles);
+    },
+
+    async getAsomeboard() {
+        const ports = await SerialPort.list();
+        console.log(ports);
+        return ports.find(port => port.manufacturer.startsWith("Silicon Labs"));
     }
 }
+
+const codeListFiles =
+`import os\r\n
+_codes_ = ""\r\n
+_codes_ = _codes_ + 'files = os.listdir("")\\n'\r\n
+_codes_ = _codes_ + 'count = 0\\n'\r\n
+_codes_ = _codes_ + 'for file in files:\\n'\r\n
+_codes_ = _codes_ + '    if file == "system.run.temp.py":\\n'\r\n
+_codes_ = _codes_ + '        continue\\n'\r\n
+_codes_ = _codes_ + '    count = count + 1\\n'\r\n
+_codes_ = _codes_ + '    print(file)\\n'\r\n
+_codes_ = _codes_ + 'print(count, "files")\\n'\r\n
+exec(_codes_)\r\n`;
