52e480f0fc323e596ba35157b00b4016767eabae
Ryu <ryujt658@gmail.com>
2022-11-03 23:37:03 +0900

    코드 에디터 닫기 전 상태 저장하기

---
+++ src/utils/latency-timer.js
@@ -0,0 +1,33 @@
+export class LatencyTimer {
+    #timer = null;
+    #count = 0;
+    #limit = 5000;
+    #onTimer = null;
+
+    constructor(limit, onTimer) {
+        this.#limit = limit;
+        this.#onTimer = onTimer;
+    }
+
+    start() {
+        this.#count = 0;
+        this.#timer = setInterval(() => {
+            this.#count += 100;
+            if (this.#count >= this.#limit) {
+                this.#count = 0;
+                this.#onTimer();
+            }
+        }, 100);
+    }
+
+    stop() {
+        if (this.#timer) {
+            clearInterval(this.#timer);
+            this.#timer = null;
+        }
+    }
+
+    reset() {
+        this.#count = 0;
+    }
+}
