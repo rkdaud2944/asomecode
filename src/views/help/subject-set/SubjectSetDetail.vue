2b6c286b8a3b15b08c34e64d9cbd84d82b2918e3
신연우 <42735839+Shinyeonwoo@users.noreply.github.com>
2023-08-16 09:47:44 +0900

    Merge branch 'feature/asomeblock' of https://bitbucket.org/asomeit/asomecode-front into feature/asomeblock

--- src/views/help/subject-set/SubjectSetDetail.vue
+++ src/views/help/subject-set/SubjectSetDetail.vue
@@ -1,33 +1,31 @@
 <template>
     <Header />
     <br>
-
     <div class="q-pa-md">
         <div>
-            <h4>
-                <span v-if="selected">{{ subjectSet.title }} (선택됨)</span>
-            </h4>
+            <div>
+            <p class="help-detail-font-style">Help 
+                <img class="help-detail-title-img-style" src="../../../../public/images/common/arrow.png">
+                <span class="color--999899">Custom Curriculum Set</span>
+            </p>
+            <input class="help-detail-title-font-style" filled readonly v-model="subjectSet.title"/><br>
         </div>
-        <br>
-
-        <div class="q-ma-md">과목 목록</div>
-        <q-table :rows="subjectSet.subjects" :columns="columns" row-key="id" hide-bottom/><br>
-
-        <div class="row">
-            <div class="col q-pr-md">
-                <q-input filled v-model="subjectSet.title" label="제목" stack-label readonly /><br>
-            </div>
-            <div class="col q-pl-md">
-                <q-input filled v-model="subjectSet.writer" label="작성자" stack-label readonly /><br>
-            </div>
+            <input class="help-detail-writer-font-style" filled readonly v-model="subjectSet.writer"/>
         </div>
 
-        <div class="row flex flex-center">
-            <q-btn @click="selectSubjetSet" color="primary" label="적용" />
-            <q-btn @click="clearSelection" color="primary" label="적용 취소" class="q-ml-md" />
+        <div class="hr">
+        </div>
+
+        <div class="q-ma-md margin-left--5">과목 목록</div>
+        <q-table class="help-detail-table-style" :rows="subjectSet.subjects" :columns="columns" row-key="id" hide-bottom/><br>
+
+        <div class="margin-right--5 text-align--right">
+            <q-btn @click="selectSubjetSet" color="primary" label="어썸코드 적용" />
+            <!-- <q-btn @click="clearSelection" color="primary" label="적용 취소" class="q-ml-md" /> -->
 
             <q-btn @click="onclickUpdateBtn" color="secondary" label="수정" class="q-ml-md" />
-            <q-btn @click="onclickDeleteBtn" color="deep-orange" label="삭제" class="q-ml-md" />
+            <q-btn @click="onclickDeleteBtn" color="red" label="삭제" class="q-ml-md" />
+            <q-btn class="q-ml-md" @click="goBack()" color="green" label="뒤로가기"  />
         </div>
     </div>
 
@@ -128,10 +126,20 @@ export default {
 }
 
 const columns = [
-{ name: 'id', align: 'center', label: 'ID', field: 'id', style: 'width: 30px' },
-    { name: 'writer', align: 'center', label: '작성자', field: 'writer', style: 'width: 150px' },
+    // { name: 'id', align: 'center', label: 'ID', field: 'id', style: 'width: 30px' },
     { name: 'title', align: 'left', label: '제목', field: 'title', style: 'width: 250px' },
     { name: 'subTitle', align: 'left', label: '부제목', field: 'subTitle', style: 'width: 200px' },
-    { name: 'description', align: 'left', label: '설명', field: 'description', style: 'width: 300px' },
+    { name: 'description', align: 'left', label: '설명', field: 'description', style: 'width: 300px',
+        format: (value) => {
+            if (value.length > 15) {
+                return value.substring(0, 15) + '...';
+            }
+            return value;
+        },
+    },
+    { name: 'writer', align: 'center', label: '작성자', field: 'writer', style: 'width: 150px' },
 ];
+
 </script>
+<style scoped src="@/assets/css/component/common.css"/>
+<style src="@/assets/css/component/SubjectDetail.css"/>
--- src/views/help/subject/SubjectList.vue
+++ src/views/help/subject/SubjectList.vue
@@ -1,34 +1,42 @@
 <template>
     <Header />
-
-    <div class="row q-pa-md">
-        <div class="col q-pr-md">
-            <q-select filled v-model="selectedFilter" :options="filterOptions" 
-            option-value="value"
-                option-label="label" label="필터" />
-        </div>
-        <div class="col q-pl-md">
-            <q-input color="teal" filled v-model="keyword" label="검색어" v-on:keyup.enter="searchSubject">
-                <template v-slot:prepend>
-                    <q-icon name="search" />
-                </template>
-            </q-input>
-        </div>
+    <div> 
+        <p class="help-main-title-font-style margin-left--5 text-align--left">
+            Custom Curriculum
+        </p>
+        <p class="help-sub-title-font-style margin-left--5 text-align--left">
+            과목을 직접 커스텀하여<br>나만의 과목을 만들어봐요
+        </p>
     </div>
+    <div class="height--95vh">
+        <div class="row q-pa-md help-search-bar">
+            <div class="custom-div1">
+                <q-select dense square filled v-model="selectedFilter" :options="filterOptions" option-value="value" option-label="label" label="필터" />
+            </div>
+            <div class="custom-div2">
+                <q-input dense square filled color="teal" v-model="keyword" label="검색어를 입력하세요" v-on:keyup.enter="searchSubject">
+                </q-input>
+            </div>
+        </div>
 
-    <div class="q-pa-md">
-        <Grid ref="grid" rowKey="id" :columns="columns" @onPageChanged="onPageChanged" @onRowClick="onRowClick" />
-        <br />
-        <div class="row flex flex-center">
-            <q-btn @click="onWriteButtonClick" color="primary" label="글쓰기" />
+        <div class="hr">
+        </div>
+
+        <div class="padding-top--16">
+            <Grid ref="grid" rowKey="id" :columns="columns" @onPageChanged="onPageChanged" @onRowClick="onRowClick" /><br />        
+            <div class="row flex flex-center text-align--right margin-right--5 display-block">
+                <q-btn @click="onWriteButtonClick" color="primary" label="만들기" />
+            </div>
         </div>
     </div>
 </template>
+
 
 <script>
 import Header from "@/components/HeaderHelp.vue";
 import apiSubject from '@/api/subject';
 import Grid from '@/components/GridPage.vue';
+
 
 export default {
     components: { Header, Grid },
@@ -90,10 +98,20 @@ export default {
 };
 
 const columns = [
-    { name: 'id', align: 'center', label: 'ID', field: 'id', style: 'width: 30px' },
-    { name: 'writer', align: 'center', label: '작성자', field: 'writer', style: 'width: 150px' },
-    { name: 'title', align: 'left', label: '제목', field: 'title', style: 'width: 250px' },
-    { name: 'subTitle', align: 'left', label: '부제목', field: 'subTitle', style: 'width: 200px' },
-    { name: 'description', align: 'left', label: '설명', field: 'description', style: 'width: 300px' },
+    // { name: 'id', align: 'center', label: 'ID', field: 'id', style: 'width: 30px' },
+    { name: 'title', align: 'center', label: '제목', field: 'title', style: 'width: 250px;' },
+    { name: 'subTitle', align: 'center', label: '부제목', field: 'subTitle', style: 'width: 200px;' },
+    { name: 'description', align: 'center', label: '설명', field: 'description', style: 'width: 300px;',
+        format: (value) => {
+            if (value.length > 15) {
+                return value.substring(0, 15) + '...';
+            }
+            return value;
+        },
+    },
+    { name: 'writer', align: 'center', label: '작성자', field: 'writer', style: 'width: 150px;' },
+    // { name: 'createat', align: 'left', label: '생성일', field: 'createat', style: 'width: 300px' },
 ];
 </script>
+<style scoped src="@/assets/css/component/common.css"/>
+<style src="@/assets/css/component/SubjectList.css"/>
