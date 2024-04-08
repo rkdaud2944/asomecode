adfc83d8b81a295b2be9d30774561b4cc29a6e80
yeonw <42735839+Shinyeonwoo@users.noreply.github.com>
2024-04-08 20:13:56 +0900

    fix: 로그 /view 기능

--- src/globals/editor-control.js
+++ src/globals/editor-control.js
@@ -1,16 +1,18 @@
 import eventbus from "@/globals/eventbus";
 import windows from "@/globals/windows";
+import router from "@/router";
 
 eventbus.on("onSerialReceived", (data) => {
     if (!data) return;
 
-         if (data.startsWith("### System.Start.View")) editorControl.clearLines();
+    if (data.startsWith("### System.Start.View")) editorControl.clearLines();
     else if (data.startsWith("### System.End.View"))   editorControl.openEditor();
     else editorControl.addLine(data);
 });
 
 let startToView = false;
 let lines = [];
+let editor = [];
 
 /**
  * 코드 에디터와 관련된 Event를 처리한다.
@@ -30,9 +32,12 @@ const editorControl = {
     },
 
     openEditor() {
+        const route = router.resolve({ path: '/editor' });
         startToView = false;
         localStorage.setItem("code", lines.join("\n"));
         windows.open('editor', '/editor');
+        editor['editor'] = window.open(route.href, 'editor', 'width=800,height=600'); // 400 300
+
     },
 };
 
