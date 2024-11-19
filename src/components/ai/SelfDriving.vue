2e675badc5cd7fefd83f3eb144024c594077d3d9
Shinyeonwoo <42735839+Shinyeonwoo@users.noreply.github.com>
2024-11-19 14:12:53 +0900

    feat: ip 입력창 추가

--- src/components/ai/SelfDriving.vue
+++ src/components/ai/SelfDriving.vue
@@ -1,33 +1,32 @@
 <template>
     <div>
         <h4>자율주행 학습하기</h4>
-        <!-- 로딩 중 메시지 -->
         <div v-if="isLoading" class="loading-message">
             Loading...
         </div>
-        <!-- 이미지 스트림 -->
         <div class="image-container">
             <img id="stream" :src="streamUrl" alt="Live Stream" width="320" height="240" @error="onImageError" @load="onImageLoad">
         </div>
-        <!-- 학습 시작 버튼 -->
         <div class="controls">
-            <button @click="openModal" class="styled-button">Wi-Fi 연결</button>
+            <input type="text" v-model="streamIp" placeholder="Enter Stream IP" class="input-ip">
+            <button @click="registerStreamIp" class="styled-button small-button">IP 등록</button>
+        </div>
+        <div class="controls">
+            <button @click="openModal" class="styled-button small-button">Wi-Fi 연결</button>
         </div>
 
-        <!-- Wi-Fi 정보 입력 모달 -->
         <div v-if="isModalOpen" class="modal">
             <div class="modal-content">
                 <h5>Wi-Fi 연결 정보 입력</h5>
                 <label for="ssid">Wi-Fi SSID:</label>
-                <input type="text" v-model="wifiSsid" id="ssid" placeholder="Enter Wi-Fi SSID">
+                <input type="text" v-model="wifiSsid" id="ssid" placeholder="Enter Wi-Fi SSID" class="small-input">
 
                 <label for="password">Wi-Fi Password:</label>
-                <!-- 비밀번호 가리지 않음 -->
-                <input type="text" v-model="wifiPassword" id="password" placeholder="Enter Wi-Fi Password">
+                <input type="text" v-model="wifiPassword" id="password" placeholder="Enter Wi-Fi Password" class="small-input">
 
                 <div class="modal-buttons">
-                    <button @click="connectWifi" class="styled-button">Connect</button>
-                    <button @click="closeModal" class="styled-button cancel-button">Cancel</button>
+                    <button @click="connectWifi" class="styled-button small-button">Connect</button>
+                    <button @click="closeModal" class="styled-button cancel-button small-button">Cancel</button>
                 </div>
             </div>
         </div>
@@ -40,14 +39,15 @@ import remoteSerial from "@/globals/remote-serial";
 export default {
     data() {
         return {
-            streamUrl: "http://192.168.0.214/stream", // 초기 스트림 URL 설정
-            isLoading: true, // 로딩 상태를 나타내는 변수
-            retryCount: 0, // 로딩 재시도 횟수
-            maxRetries: 5, // 최대 재시도 횟수
-            retryDelay: 3000, // 재시도 딜레이 (밀리초)
-            isModalOpen: false, // 모달 열림 상태
-            wifiSsid: '', // 입력받은 Wi-Fi SSID
-            wifiPassword: '' // 입력받은 Wi-Fi 비밀번호
+            streamIp: '', 
+            streamUrl: '', 
+            isLoading: true, 
+            retryCount: 0, 
+            maxRetries: 5, 
+            retryDelay: 3000, 
+            isModalOpen: false,
+            wifiSsid: '',
+            wifiPassword: ''
         };
     },
     methods: {
@@ -59,6 +59,7 @@ export default {
             this.isLoading = false; // 로딩 상태를 false로 변경
             this.retryCount = 0; // 재시도 횟수 초기화
         },
+
         // 이미지 로드 실패 시 호출되는 메서드
         onImageError() {
             if (this.retryCount < this.maxRetries) {
@@ -70,26 +71,35 @@ export default {
                 this.isLoading = true; // 재시도 실패 후 로딩 상태로 유지
             }
         },
+
         // 스트림 다시 요청하는 메서드
         retryStream() {
-            this.streamUrl = "http://192.168.0.214/stream?time=" + new Date().getTime(); // 타임스탬프 추가로 캐시 방지
+            this.streamUrl = `http://${this.streamIp}/stream?time=` + new Date().getTime(); // 타임스탬프 추가로 캐시 방지
         },
-        // 모달 열기
+
         openModal() {
             this.isModalOpen = true;
         },
-        // 모달 닫기
+
         closeModal() {
             this.isModalOpen = false;
         },
-        // 카메라 실행하기
+
         connectWifi() {
-            // 입력받은 Wi-Fi 정보로 camera_control.start() 호출
             const codes = `import camera_control\ncamera_control.start("${this.wifiSsid}", "${this.wifiPassword}")`;
             this.send(codes);
             this.closeModal();
         },
-        // 코드를 실행하는 메서드
+
+        registerStreamIp() {
+            if (this.streamIp) {
+                this.streamUrl = `http://${this.streamIp}/stream`;
+                this.isLoading = true; // 로딩 상태로 변경하여 이미지 재로딩 시작
+            } else {
+                console.error("Please enter a valid IP address");
+            }
+        },
+
         send(codes) {
             console.log("Sending codes:", codes);
             if (this.$store.state.connectionMode === 'ble') {
@@ -117,20 +127,21 @@ export default {
 
 /* 로딩 메시지 스타일 */
 .loading-message {
-    margin-top: 10px; /* 이미지와 로딩 메시지 사이에 여백 추가 */
-    color: #333; /* 로딩 메시지 색상 */
+    margin-top: 10px; 
+    color: #333; 
     font-size: 16px;
     font-weight: bold;
 }
 
-/* 컨트롤 버튼 스타일 */
 .controls {
     margin-top: 20px;
+    display: flex;
+    align-items: center;
 }
 .styled-button {
     margin-right: 10px;
-    padding: 10px 20px;
-    font-size: 16px;
+    padding: 5px 10px; 
+    font-size: 14px;
     cursor: pointer;
     background-color: #007BFF;
     color: white;
@@ -146,6 +157,11 @@ export default {
 }
 .cancel-button:hover {
     background-color: #5a6268;
+}
+
+.small-button {
+    padding: 5px 10px; 
+    font-size: 14px; 
 }
 
 /* 모달 스타일 */
@@ -172,9 +188,18 @@ export default {
     display: flex;
     justify-content: space-between;
 }
+.input-ip {
+    padding: 5px;
+    margin-right: 10px;
+    font-size: 14px;
+}
+.small-input {
+    padding: 5px;
+    font-size: 14px;
+}
 input {
     width: 100%;
-    padding: 10px;
+    padding: 5px;
     margin-top: 10px;
     box-sizing: border-box;
     font-size: 14px;
