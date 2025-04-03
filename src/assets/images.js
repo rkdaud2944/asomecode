4a4a65182cac4f53d921273b992ec3619545c3f5
Shinyeonwoo <42735839+Shinyeonwoo@users.noreply.github.com>
2025-04-03 10:15:01 +0900

    chore: 머지 조정

--- src/assets/images.js
+++ src/assets/images.js
@@ -20,16 +20,16 @@ import update from "@/assets/images/controls/update.png";
 import help from "@/assets/images/controls/help.png";
 import setting from "@/assets/images/controls/setting.png";
 import menu from "@/assets/images/controls/menu.png";
-import block from "@/assets/images/controls/block.png";
+// import block from "@/assets/images/controls/block.png";
 
-import connect_default from "@/assets/images/controls/navbar/connect_default.png";
-import connect_hover from "@/assets/images/controls/navbar/connect_hover.png";
-import connect_click from "@/assets/images/controls/navbar/connect_click.png";
-import connect_variant from "@/assets/images/controls/navbar/connect_variant.png";
-import stop_default from "@/assets/images/controls/navbar/stop_default.png";
-import stop_hover from "@/assets/images/controls/navbar/stop_hover.png";
-import stop_click from "@/assets/images/controls/navbar/stop_click.png";
-import block from "@/assets/images/controls/block.png";
+// import connect_default from "@/assets/images/controls/navbar/connect_default.png";
+// import connect_hover from "@/assets/images/controls/navbar/connect_hover.png";
+// import connect_click from "@/assets/images/controls/navbar/connect_click.png";
+// import connect_variant from "@/assets/images/controls/navbar/connect_variant.png";
+// import stop_default from "@/assets/images/controls/navbar/stop_default.png";
+// import stop_hover from "@/assets/images/controls/navbar/stop_hover.png";
+// import stop_click from "@/assets/images/controls/navbar/stop_click.png";
+// import block from "@/assets/images/controls/block.png";
 
 import runEdt from "@/assets/images/editor/run.png";
 import openEdt from "@/assets/images/editor/open.png";
--- src/blocks/blockcontents_car.js
+++ src/blocks/blockcontents_car.js
@@ -135,22 +135,6 @@ const CarToolbox = {
                 { "kind": "block", "gap": "8", "type": "variable" },
             ],
         },
-<<<<<<< HEAD
-        // {
-        //     "kind": "category",
-        //     "name": "AI",
-        //     "field" : "CAR",
-        //     "colour": "5058D1",
-        //     "contents": [
-        //         // { "kind": "block", "gap": "8", "type": "ai_stt" },
-        //         // { "kind": "block", "gap": "8", "type": "ai_tts" },
-        //         // { "kind": "block", "gap": "8", "type": "ai_tts_play" },
-        //         // { "kind": "block", "gap": "8", "type": "ai_tts_delete" },
-        //         // { "kind": "block", "gap": "8", "type": "stt_mark_int" },
-        //     ],
-        // },
-=======
->>>>>>> feature/serial
         {
             "kind": "category",
             "name": "INTERNET",
